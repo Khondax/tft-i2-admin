c47e841131695790094bbc5f42bc8f3e5f974a9d
unknown <mariaguerra803@gmail.com>
2017-04-13 19:23:47 +0100

    Order Slides

--- src/pages/order/order.page.ts
+++ src/pages/order/order.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController, NavParams } from 'ionic-angular';
 
+import { AngularFire, FirebaseListObservable } from "angularfire2";
+
 import { MapPage } from '../pages';
 
  @Component ({
@@ -10,13 +12,25 @@ import { MapPage } from '../pages';
  export class OrderPage {
 
     order: any;
+    deliveryMen: FirebaseListObservable<any>;
 
-    constructor(private nav: NavController, private navParams: NavParams){
+    constructor(private nav: NavController, 
+                private navParams: NavParams,
+                private angularFire: AngularFire){
+
         this.order = this.navParams.data;
+        this.deliveryMen = angularFire.database.list('/repartidores');
      }
+
 
     goToMap(){
     this.nav.push(MapPage, this.order);
     }
+
+getCorrectColor(deliveryMan){
+    return deliveryMan.disponibilidad === "Ocupado" ? 'primary' : 'verde';
+  }
+
+
 
  }
