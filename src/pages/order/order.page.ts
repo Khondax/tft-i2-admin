42aca82976f0c29e63e414a5b901a2d7d78d26bc
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-04-13 20:01:14 +0100

    Dialogo para añadir un repartidor a un paquete

--- src/pages/order/order.page.ts
+++ src/pages/order/order.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController, NavParams } from 'ionic-angular';
+import { NavController, LoadingController, NavParams, AlertController } from 'ionic-angular';
 
 import { AngularFire, FirebaseListObservable } from "angularfire2";
 
@@ -16,7 +16,8 @@ import { MapPage } from '../pages';
 
     constructor(private nav: NavController, 
                 private navParams: NavParams,
-                private angularFire: AngularFire){
+                private angularFire: AngularFire,
+                private alertController: AlertController){
 
         this.order = this.navParams.data;
         this.deliveryMen = angularFire.database.list('/repartidores');
@@ -24,13 +25,43 @@ import { MapPage } from '../pages';
 
 
     goToMap(){
-    this.nav.push(MapPage, this.order);
+        this.nav.push(MapPage, this.order);
     }
 
-getCorrectColor(deliveryMan){
-    return deliveryMan.disponibilidad === "Ocupado" ? 'primary' : 'verde';
-  }
+    getCorrectColor(deliveryMan){
+        return deliveryMan.disponibilidad === "Ocupado" ? 'primary' : 'verde';
+    }
 
+    assignDeliveryMan($event, deliveryMan){
+        /*  TODO: Editar base de datos para añadir repartidor al paquete seleccionado
+        *   Editar base de datos para que el repartidor tenga otro paquete
+        *   En HomePage, los paquetes con repartidor no pueden verse (Refresher automático) 
+        *   Añadir Toast para indicar que el repartidor se ha añadido al pedido
+        */
+
+        let prompt = this.alertController.create({
+            title: 'Asignar',
+            message: "¿Quieres añadir el repartidor " + deliveryMan.nombre + " al pedido? ",
+            buttons: [
+                {
+                    text: 'Cancelar',
+                    handler: data => {
+                        console.log('Cancelado');
+                    }
+                },
+                {
+                    text: 'Si',
+                    handler: data =>{
+                        //TODO: cosas
+                        this.nav.popToRoot();
+                    }
+                }
+            ]
+        });
+
+        prompt.present();
+
+    }
 
 
  }
