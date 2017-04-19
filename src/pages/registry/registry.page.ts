73d4b91a24c8ffb4d1e11b73bb8b56ed8f06836f
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-04-19 12:14:23 +0100

    Historial muestra pedidos según fecha de entrega (corregir pedidos sin fecha)

--- src/pages/registry/registry.page.ts
+++ src/pages/registry/registry.page.ts
@@ -3,7 +3,9 @@ import { NavController, LoadingController, AlertController } from 'ionic-angular
 
 import { AngularFire, FirebaseListObservable } from "angularfire2";
 
-import {  } from '../pages';
+import _ from 'lodash';
+
+import { OrderPage } from '../pages';
 
  @Component ({
      templateUrl: 'registry.page.html'
@@ -11,17 +13,54 @@ import {  } from '../pages';
 
  export class RegistryPage {
 
-    orders: FirebaseListObservable<any>;
+    //orders: FirebaseListObservable<any>;
+    orders = [];
+    private allOrders: any;
+    private allDates: any;
+
 
     constructor(public nav: NavController,
                 public alertController: AlertController,
-                public angularFire: AngularFire){
-
-        this.orders = angularFire.database.list('/pedidos');
+                public angularFire: AngularFire,
+                public loadingController: LoadingController){
 
     }
 
     //TODO Importar datos de paquetes entregados de firebase
+
+    ionViewDidLoad(){
+
+        let loader = this.loadingController.create ({
+            content: 'Obteniendo datos...'
+        });
+
+        loader.present().then(() => {
+            this.angularFire.database.list('/pedidos').subscribe(data => {
+                this.allOrders = data;
+                this.allDates = 
+                    _.chain(data)
+                    .groupBy('fechaEntrega')
+                    .toPairs()
+                    .map(item => _.zipObject(['date', 'order'],
+                    item))
+                    .value();
+
+                this.orders = this.allDates;
+                console.log('Datos o yo que se', this.orders);
+                loader.dismiss();
+
+            });
+
+
+        });
+
+
+    }
+
+    itemTapped($event, order){
+
+        this.nav.push(OrderPage, order);
+    }
 
 
  }
