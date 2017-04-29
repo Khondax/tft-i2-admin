5be62c31dcaba1df903b2622fa3a28342d942dcd
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-04-29 11:52:30 +0100

    Nueva página para ver localización de repartidores

---
+++ src/pages/deliverLocation/deliverLocation.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+ @Component ({
+     templateUrl: 'deliverLocation.page.html',
+ })
+
+ export class DeliverLocationPage {
+
+    constructor(public nav: NavController){
+
+    }
+
+
+
+
+ }
