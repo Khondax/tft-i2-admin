cf5ae6bea194f6df91ebbf515571ae7fd38c49d6
unknown <mariaguerra803@gmail.com>
2017-05-29 12:49:14 +0100

    Mapa en tab asignados de repartidores.

--- src/pages/delivererPacks/delivererPacks.page.ts
+++ src/pages/delivererPacks/delivererPacks.page.ts
@@ -16,6 +16,7 @@ import { OrderPage } from "../pages";
     ordersData: any;
     orders = [];
     deliverer: any = {};
+    map: any = {};
 
     constructor (public nav: NavController,
                  public navParams: NavParams,
@@ -25,6 +26,12 @@ import { OrderPage } from "../pages";
 
     ionViewDidLoad(){
          this.deliverer = this.navParams.data;
+
+         this.map = {
+                    lat: 27.942246703329612,
+                    lng: -15.598526000976562,
+                    zoom: 9
+            };
 
          this.angularFire.database.list('/pedidos').subscribe(data => {
 
