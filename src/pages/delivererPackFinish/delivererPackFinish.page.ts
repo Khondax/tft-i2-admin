282086f8c56926c20f6975da9c8146ff01bc4174
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-06-27 13:04:26 +0100

    Optimización del código

--- src/pages/delivererPackFinish/delivererPackFinish.page.ts
+++ src/pages/delivererPackFinish/delivererPackFinish.page.ts
@@ -27,25 +27,17 @@ export class DelivererPackFinishPage {
 
         this.angularFire.database.list('/pedidos').subscribe(data => {
                 this.ordersData = _.chain(data)
+                                  .filter(o => o.idRepartidor === this.deliverer.$key && o.fechaEntrega != "")
                                   .groupBy('codigoPostal')
                                   .toPairs()
                                   .map(item => _.zipObject(['codPos', 'pedido'], item))
                                   .value();
 
-                var index = 0;
-                for(var i = 0; i < this.ordersData.length; i++){
-                    var k = 0;
-                    var temp = [];
-                    for(var j = 0; j < this.ordersData[i].pedido.length; j++){
-                        if(this.ordersData[i].pedido[j].fechaEntrega !="" && this.ordersData[i].pedido[j].idRepartidor == this.deliverer.$key){
-                            temp[k] = this.ordersData[i].pedido[j];
-                            k++;
-                        }
-                    }
 
-                    this.orders[index] = temp;
-                    index++;
-                }
+                this.orders = _.chain(this.ordersData)
+                                    .orderBy('direccion')
+                                    .value();
+
         });
     }
 
